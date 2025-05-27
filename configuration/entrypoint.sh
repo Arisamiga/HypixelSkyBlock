45e00aad71fb1286d96b86e106b7ccbaf96cc5c6
Aris <64918822+Arisamiga@users.noreply.github.com>
2025-05-27 19:12:23 +0100

    (WIP) Making Docker containers Modular

--- configuration/entrypoint.sh
+++ configuration/entrypoint.sh
@@ -12,21 +12,5 @@ mv ./configuration/resources.json.tmp ./configuration/resources.json
 sed -i "s/secret: '.*'/secret: '$secret'/" ./settings.yml
 
 
-# Clear any previously created Screens (Starting freshhh)
-screen -wipe
-
-# Start services in separate screen sessions
-screen -dmS SkyBlockCore_ISLAND java --enable-preview -jar SkyBlockCore.jar ISLAND
-sleep 20
-screen -dmS SkyBlockCore_HUB java --enable-preview -jar SkyBlockCore.jar HUB
-screen -dmS SkyBlockCore_FARMING java --enable-preview -jar SkyBlockCore.jar THE_FARMING_ISLANDS
-screen -dmS NanoLimbo java -jar NanoLimbo-1.9.1-all.jar
-screen -dmS ServiceAPI java -jar ServiceAPI.jar
-screen -dmS ServiceAuctionHouse java -jar ServiceAuctionHouse.jar
-screen -dmS ServiceBazaar java -jar ServiceBazaar.jar
-screen -dmS ServiceItemTracker java -jar ServiceItemTracker.jar
-
-echo "Started all services with a total of: $(screen -ls | grep -c 'Detached') screens"
-
-# Keep the container running
-tail -f /dev/null
+# Run the Java application with optional pre-arguments and environment variables
+java $PREVIEW_FLAG -jar "$JAR_FILE" $ARGS
